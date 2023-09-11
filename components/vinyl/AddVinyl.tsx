5c0c577be7f64a20f829d0c5e45198c74ab9c51f
Kyle Scudder <74150974+kylescudder@users.noreply.github.com>
2023-09-11 09:28:09 +0100

    Added back button to AddVinyl form

--- components/vinyl/AddVinyl.tsx
+++ components/vinyl/AddVinyl.tsx
@@ -10,6 +10,7 @@ import { updateVinyl } from "@/lib/actions/vinyl.action";
 import Checkbox from "../ui/checkbox";
 import { Button } from "../ui/button";
 import { successToast } from "@/lib/actions/toast.actions";
+import { IconArrowNarrowLeft } from "@tabler/icons-react";
 
 export default function AddVinyl(props: { vinyl: IVinyl }) {
   const router = useRouter();
@@ -48,69 +49,84 @@ export default function AddVinyl(props: { vinyl: IVinyl }) {
       userGroupID: "",
     };
     const vinyl = await updateVinyl(payload);
-    if (pathname.includes('/vinyl/')) {
+    if (pathname.includes("/vinyl/")) {
       successToast(vinyl);
     } else {
       router.push(`/vinyl/${vinyl._id}`);
     }
   };
 
+  const handleBack = () => {
+    router.back();
+  };
+
   return (
-    <Form {...form}>
-      <form
-        onSubmit={form.handleSubmit(onSubmit)}
-        className="flex flex-col justify-start gap-10"
-      >
-        <FormField
-          control={form.control}
-          name="name"
-          render={({ field }) => (
-            <FormItem className="flex flex-col gap-3 w=full">
-              <FormLabel className="text-base-semibold text-dark-2 dark:text-light-2">
-                Name
-              </FormLabel>
-              <FormControl>
-                <Input
-                  type="text"
-                  className="account-form_input no-focus text-dark-2 dark:text-light-2"
-                  {...field}
-                />
-              </FormControl>
-            </FormItem>
-          )}
-        />
-        <FormField
-          control={form.control}
-          name="artistName"
-          render={({ field }) => (
-            <FormItem className="flex flex-col gap-3 w=full">
-              <FormLabel className="text-base-semibold text-dark-2 dark:text-light-2">
-                Artist
-              </FormLabel>
-              <FormControl>
-                <Input
-                  type="text"
-                  className="account-form_input no-focus text-dark-2 dark:text-light-2"
-                  {...field}
-                />
-              </FormControl>
-            </FormItem>
-          )}
-        />
-        <FormField
-          name="purchased"
-          render={({ field }) => (
-            <FormItem className="flex flex-col gap-3 w=full">
-              <FormControl>
-                <Checkbox text="Purchased" {...field} />
-              </FormControl>
-            </FormItem>
-          )}
-        />
-        <Button className="bg-primary-500 text-light-1" type="submit">
-          Add Vinyl
-        </Button>
-      </form>
-    </Form>
+    <div>
+      <IconArrowNarrowLeft
+        onClick={handleBack}
+        aria-label="back"
+        className={`dark:text-light-1 text-dark-1 ${
+          props.vinyl._id === "" ? "hidden" : ""
+        }`}
+      />
+      <Form {...form}>
+        <form
+          onSubmit={form.handleSubmit(onSubmit)}
+          className={`flex flex-col justify-start gap-10 pt-4 ${
+            props.vinyl._id === "" ? "px-6" : ""
+          }`}
+        >
+          <FormField
+            control={form.control}
+            name="name"
+            render={({ field }) => (
+              <FormItem className="flex flex-col gap-3 w=full">
+                <FormLabel className="text-base-semibold text-dark-2 dark:text-light-2">
+                  Name
+                </FormLabel>
+                <FormControl>
+                  <Input
+                    type="text"
+                    className="account-form_input no-focus text-dark-2 dark:text-light-2"
+                    {...field}
+                  />
+                </FormControl>
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="artistName"
+            render={({ field }) => (
+              <FormItem className="flex flex-col gap-3 w=full">
+                <FormLabel className="text-base-semibold text-dark-2 dark:text-light-2">
+                  Artist
+                </FormLabel>
+                <FormControl>
+                  <Input
+                    type="text"
+                    className="account-form_input no-focus text-dark-2 dark:text-light-2"
+                    {...field}
+                  />
+                </FormControl>
+              </FormItem>
+            )}
+          />
+          <FormField
+            name="purchased"
+            render={({ field }) => (
+              <FormItem className="flex flex-col gap-3 w=full">
+                <FormControl>
+                  <Checkbox text="Purchased" {...field} />
+                </FormControl>
+              </FormItem>
+            )}
+          />
+          <Button className="bg-primary-500 text-light-1" type="submit">
+            Add Vinyl
+          </Button>
+        </form>
+      </Form>
+    </div>
   );
 }
