8dd2e3673bc1f8cfae7e4ec03fdf48dc050cdf51
Kyle Scudder <74150974+kylescudder@users.noreply.github.com>
2023-09-11 22:59:30 +0100

    Moved main-container tailwindcss inline

--- components/shared/MainContent.tsx
+++ components/shared/MainContent.tsx
@@ -1,21 +1,21 @@
-"use client"
+"use client";
 
 import CustomThemeProvider from "@/components/shared/CustomThemeProvider";
 import LeftSidebar from "./LeftSidebar";
 import { IPot } from "@/lib/models/pot";
 
 export const MainContent = (props: {
-	pots: IPot[]
-	children: React.ReactNode;
+  pots: IPot[];
+  children: React.ReactNode;
 }) => {
-	return (
-		<CustomThemeProvider>
-			<main className="flex flex-row">
-				<LeftSidebar pots={props.pots} />
-				<section className="main-container">
-					<div className="w-full max-w-4xl">{props.children}</div>
-				</section>
-			</main>
-		</CustomThemeProvider>
-	);
+  return (
+    <CustomThemeProvider>
+      <main className="flex flex-row">
+        <LeftSidebar pots={props.pots} />
+        <section className="flex min-h-screen flex-1 flex-col items-center bg-light-1 dark:bg-dark-1 px-6 pb-10 pt-16 max-md:pb-32 sm:px-10">
+          <div className="w-full max-w-4xl min-h-full">{props.children}</div>
+        </section>
+      </main>
+    </CustomThemeProvider>
+  );
 };
