924b52e2fcebe4522bda84cac3f5d0c21a2cac46
ScudderK <74150974+kylescudder@users.noreply.github.com>
2023-09-12 21:11:04 +0100

    Made login page follow dark mode scheme

--- app/(auth)/sign-in/[[...sign-in]]/page.tsx
+++ app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -2,7 +2,7 @@ import { SignIn } from "@clerk/nextjs";
 
 export default function Page() {
   return (
-    <div className="flex items-center justify-center h-screen">
+    <div className="flex items-center justify-center h-screen bg-dark-1">
       <SignIn />
     </div>
   );
