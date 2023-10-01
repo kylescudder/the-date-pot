e3b835dd5b7cbfd5911e18d9e36f8806d85422bb
Kyle Scudder <74150974+kylescudder@users.noreply.github.com>
2023-10-01 17:24:50 +0100

    feat: when addressed saved map now shows

---
+++ components/shared/ReloadMapPlaceholder.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { IconRefresh } from "@tabler/icons-react";
+import { useRouter } from "next/navigation";
+
+const ReloadMapPlaceholder = () => {
+	const router = useRouter();
+
+	const handleClick = () => {
+		router.refresh();
+	}
+  return (
+    <div className="flex flex-col items-center justify-center w-full h-400 bg-gray-300 rounded-md border border-gray-400">
+      <IconRefresh className="text-gray-600 text-2xl" />
+      <p onClick={handleClick} className="mt-2">To view the map, click here</p>
+    </div>
+  );
+};
+
+export default ReloadMapPlaceholder;
