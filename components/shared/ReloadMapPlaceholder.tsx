57148b20c4b8332d8746561c461a0dc9598a2c50
Kyle Scudder <kyle@kylescudder.co.uk>
2024-01-14 23:02:21 +0000

    fix(maps): #168 fixed maps issues

--- components/shared/ReloadMapPlaceholder.tsx
+++ components/shared/ReloadMapPlaceholder.tsx
@@ -9,7 +9,7 @@ const ReloadMapPlaceholder = () => {
 		router.refresh();
 	}
   return (
-    <div className="flex flex-col items-center justify-center w-full h-400 bg-gray-300 rounded-md border border-gray-400">
+    <div className="flex flex-col items-center justify-center w-full h-400 bg-gray-300 rounded-md border border-gray-400 cursor-pointer">
       <IconRefresh className="text-gray-600 text-2xl" />
       <p onClick={handleClick} className="mt-2">To view the map, click here</p>
     </div>
