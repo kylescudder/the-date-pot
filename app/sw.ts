a51bb5f84334e794d8ae053aea56ff08dcc39684
Kyle Scudder <kyle@kylescudder.co.uk>
2024-04-02 21:23:12 +0100

    fix(sw): Updated SerWist Scope type

--- app/sw.ts
+++ app/sw.ts
@@ -2,7 +2,7 @@ import { defaultCache } from '@serwist/next/browser'
 import type { PrecacheEntry } from '@serwist/precaching'
 import { installSerwist } from '@serwist/sw'
 
-declare const self: ServiceWorkerGlobalScope & {
+declare const self: ServiceWorkerGlobalScopeEventMap & {
   // Change this attribute's name to your `injectionPoint`.
   // `injectionPoint` is an InjectManifest option.
   // See https://serwist.pages.dev/docs/build/inject-manifest/configuring
