cb51cdf33a54ee3d4fb12d09d9d22b290c176b00
Kyle Scudder <kyle@kylescudder.co.uk>
2024-05-13 21:44:30 +0100

    fix(sw): updated serwist swSrc path

--- next.config.js
+++ next.config.js
@@ -37,7 +37,7 @@ module.exports = async (phase) => {
     const withSerwist = (await import('@serwist/next')).default({
       // Note: This is only an example. If you use Pages Router,
       // use something else that works, such as "service-worker/index.ts".
-      swSrc: 'app/sw.ts',
+      swSrc: 'src/app/sw.ts',
       swDest: 'public/sw.js'
     })
     return withSerwist(nextConfig)
