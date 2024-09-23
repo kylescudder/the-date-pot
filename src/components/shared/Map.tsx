7fcac41040baadcc28ff26c553b86ab9e9c9c986
Kyle Scudder <kyle@kylescudder.co.uk>
2024-09-23 22:27:54 +0100

    fix(maps): replaced fitBounds with setCenter to resolve default zoom bug

--- src/components/shared/Map.tsx
+++ src/components/shared/Map.tsx
@@ -17,7 +17,7 @@ const Map = (props: { longLat: number[]; title: string }) => {
   const onLoad = React.useCallback(function callback(map: any) {
     // This is just an example of getting and using the map instance!!! don't just blindly copy!
     const bounds = new window.google.maps.LatLngBounds(center)
-    map.fitBounds(bounds)
+    map.setCenter(bounds)
 
     setMap(map)
   }, [])
